5a8fc91960a6af820793eb7cb6cf467a4c53aa6c
Timothee Dravet <timothee.dravet@gmail.com>
2021-09-09 16:56:31 +0200

    add site list

---
+++ site_list.js
@@ -0,0 +1,6 @@
+list = {
+    "Algorigramme_Sort" : {
+        "repo_link" : "https://github.com/Tablatim/Algorigramme_Sort",
+        "gitpage_link" : "https://tablatim.github.io/Algorigramme_Sort"
+    }
+}
