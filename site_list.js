64a8e95d4c2e3b43818fddd4fc805a2441d0b9a0
Timothee Dravet <timothee.dravet@gmail.com>
2021-09-09 17:55:23 +0200

    update

--- site_list.js
+++ site_list.js
@@ -3,5 +3,15 @@ list = [
         "name" : "Algorigramme Sort",
         "repo_link" : "https://github.com/Tablatim/Algorigramme_Sort",
         "gitpage_link" : "https://tablatim.github.io/Algorigramme_Sort"
+    },
+    {
+        "name" : "3x+1 Graphic",
+        "repo_link" : "https://github.com/Tablatim/3x-1",
+        "gitpage_link" : "https://tablatim.github.io/3x-1/"
+    },
+    {
+        "name" : "Enigma",
+        "repo_link" : "https://github.com/Tablatim/Enigma",
+        "gitpage_link" : "https://tablatim.github.io/Enigma/"
     }
 ]
