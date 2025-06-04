e90ccd6f0be225f4ff5b35a69f9b2aed5618c403
Timothée <60846476+TimotheeDvt@users.noreply.github.com>
2025-06-04 10:39:04 +0200

    Update site_list.js for new username

--- site_list.js
+++ site_list.js
@@ -1,56 +1,56 @@
 list = [
     {
         "name" : "Algorigramme Sort",
-        "repo_link" : "https://github.com/Tablatim/Algorigramme_Sort",
-        "gitpage_link" : "https://tablatim.github.io/Algorigramme_Sort"
+        "repo_link" : "https://github.com/TimotheeDvt/Algorigramme_Sort",
+        "gitpage_link" : "https://timotheedvt.github.io/Algorigramme_Sort"
     },{
         "name" : "Hanoi Visualisation",
-        "repo_link" : "https://github.com/Tablatim/HanoiVisualisation",
-        "gitpage_link" : "https://tablatim.github.io/HanoiVisualisation/"
+        "repo_link" : "https://github.com/TimotheeDvt/HanoiVisualisation",
+        "gitpage_link" : "https://timotheedvt.github.io/HanoiVisualisation/"
     },
     {
         "name" : "3x+1 Graphic",
-        "repo_link" : "https://github.com/Tablatim/3x-1",
-        "gitpage_link" : "https://tablatim.github.io/3x-1/"
+        "repo_link" : "https://github.com/TimotheeDvt/3x-1",
+        "gitpage_link" : "https://timotheedvt.github.io/3x-1/"
     },
     {
         "name" : "Enigma",
-        "repo_link" : "https://github.com/Tablatim/Enigma",
-        "gitpage_link" : "https://tablatim.github.io/Enigma/"
+        "repo_link" : "https://github.com/TimotheeDvt/Enigma",
+        "gitpage_link" : "https://timotheedvt.github.io/Enigma/"
     },
     {
         "name" : "Chess",
-        "repo_link" : "https://github.com/Tablatim/Chess",
-        "gitpage_link" : "https://tablatim.github.io/Chess/"
+        "repo_link" : "https://github.com/TimotheeDvt/Chess",
+        "gitpage_link" : "https://timotheedvt.github.io/Chess/"
     },
     {
         "name" : "Pong",
-        "repo_link" : "https://github.com/Tablatim/Pong",
-        "gitpage_link" : "https://tablatim.github.io/Pong/"
+        "repo_link" : "https://github.com/TimotheeDvt/Pong",
+        "gitpage_link" : "https://timotheedvt.github.io/Pong/"
     },
     {
         "name" : "Random Backing Track",
-        "repo_link" : "https://github.com/Tablatim/RandomBackinTrack",
-        "gitpage_link" : "https://tablatim.github.io/RandomBackinTrack/"
+        "repo_link" : "https://github.com/TimotheeDvt/RandomBackinTrack",
+        "gitpage_link" : "https://timotheedvt.github.io/RandomBackinTrack/"
     },
     {
         "name" : "Snake",
-        "repo_link" : "https://github.com/Tablatim/Snake",
-        "gitpage_link" : "https://tablatim.github.io/Snake/"
+        "repo_link" : "https://github.com/TimotheeDvt/Snake",
+        "gitpage_link" : "https://timotheedvt.github.io/Snake/"
     },
     {
         "name" : "Snake-Ai",
-        "repo_link" : "https://github.com/Tablatim/Snake-Ai",
-        "gitpage_link" : "https://tablatim.github.io/Snake-Ai/"
+        "repo_link" : "https://github.com/TimotheeDvt/Snake-Ai",
+        "gitpage_link" : "https://timotheedvt.github.io/Snake-Ai/"
     },
     {
         "name" : "Gamme Note Accords Guitare & Piano",
-        "repo_link" : "https://github.com/Tablatim/Gamme-Note-Guitare",
-        "gitpage_link" : "https://tablatim.github.io/Gamme-Note-Guitare/"
+        "repo_link" : "https://github.com/TimotheeDvt/Gamme-Note-Guitare",
+        "gitpage_link" : "https://timotheedvt.github.io/Gamme-Note-Guitare/"
     },
     {
         "name" : "RayCasting 3D game",
-        "repo_link" : "https://github.com/Tablatim/RayCasting",
-        "gitpage_link" : "https://tablatim.github.io/RayCasting/"
+        "repo_link" : "https://github.com/TimotheeDvt/RayCasting",
+        "gitpage_link" : "https://timotheedvt.github.io/RayCasting/"
     }
 ]
